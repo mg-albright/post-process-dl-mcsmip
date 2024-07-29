4e2e014c7cf4ff9b02e839aa50acbd57e76fffca
Mary Grace Albright <89799653+mg-albright@users.noreply.github.com>
2024-07-29 14:17:37 -0400

    Update split_tracks.py

--- split_tracks.py
+++ split_tracks.py
@@ -3,14 +3,14 @@ import xarray as xr
 import numpy as np
 
 # get model name
-model = str(sys.argv[1])
+model = 'OBS' # Update based on model or create loop to go through all models
 
 # Load tracking NetCDF file
 path = f"/glade/derecho/scratch/malbright/mcsmip/tracking/summer/{model}/"
 minsize = 1
 minoverlap = 50
 maxoverlap = 99
-thresh = str(sys.argv[2])
+thresh = 0.03
 radius = 0.0
 file = f"blobs_summer_{model}_mintime4_minsize{minsize}_overlap{minoverlap}-{maxoverlap}_thresh{thresh}_radius{radius}"
 ds = xr.open_dataset(path + file + ".nc")
@@ -18,7 +18,6 @@ ds = xr.open_dataset(path + file + ".nc")
 # split mcs ids into chunks
 ds_na = ds["MCS_mcs_prob"].where(ds > 0)
 mcs_ids = np.unique(ds_na.MCS_mcs_prob.data[~np.isnan(ds_na.MCS_mcs_prob.data)])
-del ds_na
 
 num_chunks = 3 # choose number of chunks 
 chunks = np.array_split(mcs_ids, num_chunks)
