de6a6bc0182fc92af6631eb616b643eae645081b
Mary Grace Albright <89799653+mg-albright@users.noreply.github.com>
2024-07-29 12:41:47 -0400

    Create post-process.py
    Final post-processing if manual parallelization is needed

---
+++ post-process.py
@@ -0,0 +1,30 @@
+import sys
+import pathlib
+import xarray as xr
+import numpy as np
+
+model = str(sys.argv[1])
+thresh = 0.03
+
+data_dir = pathlib.Path(
+    f"/glade/derecho/scratch/malbright/mcsmip/tracking/summer/{model}"
+)
+files = sorted(
+    data_dir.glob(
+        f"blobs_summer_{model}_mintime4_minsize1_overlap50-99_thresh{thresh}_radius0.0_FILTERED_part*.nc"
+    )
+)
+
+product = xr.open_dataset(files[0])["MCS_mcs_prob"]
+for file in files[1:]:
+    product = product * xr.open_dataset(file)["MCS_mcs_prob"]
+    product = product.where(product <= 0, 1)
+
+path = f"/glade/derecho/scratch/malbright/mcsmip/tracking/summer/{model}"
+ds = xr.open_dataset(
+    f"{path}/blobs_summer_{model}_mintime4_minsize1_overlap50-99_thresh{thresh}_radius0.0.nc"
+)
+final_filtered = product * ds.MCS_mcs_prob
+final_filtered.to_netcdf(
+    f"{path}/blobs_summer_{model}_mintime4_minsize1_overlap50-99_thresh{thresh}_radius0.0_FILTERED.nc",
+)
